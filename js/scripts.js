e6bd2a8a88c73fed0a32eed79530ec9a5ceaa7dc
Erik Barron <todayisagrnday21@gmail.com>
2020-01-15 15:58:12 -0800

    provide a website for a bar that has an age verification prompt and will either load a menu if user is over 21 or load a message if user is under 21. if user is exactly 21, website will congratulate using an alert

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
   var age = parseInt(prompt("How old are you?"));
 
-  if (age >= 21) {
-    $('#drinks').show();
+  if (age > 21) {
+    $("#drinks").show();
+  } else if (age === 21) {
+    alert("Happy 21st!");
+    $("#drinks").show();
+  } else {
+    $("#under-21").show();
   }
+
+
 });
